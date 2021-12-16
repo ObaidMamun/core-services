97a78c08ee1028d428907c8fc57b4d16d96891c4
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2021-12-16 07:51:34 +0000

    Update StorageService.java

--- egov-filestore/src/main/java/org/egov/filestore/domain/service/StorageService.java
+++ egov-filestore/src/main/java/org/egov/filestore/domain/service/StorageService.java
@@ -1,150 +1,135 @@
 package org.egov.filestore.domain.service;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
-
 import org.egov.filestore.domain.exception.EmptyFileUploadRequestException;
 import org.egov.filestore.domain.model.Artifact;
 import org.egov.filestore.domain.model.FileInfo;
 import org.egov.filestore.domain.model.FileLocation;
 import org.egov.filestore.domain.model.Resource;
+import org.egov.filestore.persistence.entity.Artifact;
 import org.egov.filestore.persistence.repository.ArtifactRepository;
 import org.egov.filestore.repository.CloudFilesManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Service
-@Slf4j
 public class StorageService {
-
-	@Value("${is.bucket.fixed}")
-	private Boolean isBucketFixed;
-
-	@Value("${fixed.bucketname}")
-	private String fixedBucketName;
-
-	@Value("${isS3Enabled}")
-	private Boolean isS3Enabled;
-
-	@Value("${isNfsStorageEnabled}")
-	private Boolean isNfsStorageEnabled;
-
-	@Value("${isAzureStorageEnabled}")
-	private Boolean isAzureStorageEnabled;
-
-	@Value("${source.disk}")
-	private String diskStorage;
-
-	@Value("${source.s3}")
-	private String awsS3Source;
-
-	@Value("${source.azure.blob}")
-	private String azureBlobSource;
-
-	@Autowired
-	private CloudFilesManager cloudFilesManager;
-
-	private static final String UPLOAD_MESSAGE = "Received upload request for "
-			+ "jurisdiction: %s, module: %s, tag: %s with file count: %s";
-
-	private ArtifactRepository artifactRepository;
-	private IdGeneratorService idGeneratorService;
-
-	@Autowired
-	public StorageService(ArtifactRepository artifactRepository, IdGeneratorService idGeneratorService) {
-		this.artifactRepository = artifactRepository;
-		this.idGeneratorService = idGeneratorService;
-	}
-
-	public List<String> save(List<MultipartFile> filesToStore, String module, String tag, String tenantId,
-			String inputStreamAsString) {
-		validateFilesToUpload(filesToStore, module, tag, tenantId);
-		log.info(UPLOAD_MESSAGE, module, tag, filesToStore.size());
-		List<Artifact> artifacts = mapFilesToArtifacts(filesToStore, module, tag, tenantId, inputStreamAsString);
-		return this.artifactRepository.save(artifacts);
-	}
-
-	private void validateFilesToUpload(List<MultipartFile> filesToStore, String module, String tag, String tenantId) {
-		if (CollectionUtils.isEmpty(filesToStore)) {
-			throw new EmptyFileUploadRequestException(module, tag, tenantId);
-		}
-	}
-
-	private List<Artifact> mapFilesToArtifacts(List<MultipartFile> files, String module, String tag, String tenantId,
-			String inputStreamAsString) {
-
-		final String folderName = getFolderName(module, tenantId);
-		return files.stream().map(file -> {
-			String fileName = folderName + System.currentTimeMillis() + file.getOriginalFilename();
-			//String fileName = file.getOriginalFilename();
-			String id = this.idGeneratorService.getId();
-			FileLocation fileLocation = new FileLocation(id, module, tag, tenantId, fileName, null);
-			return new Artifact(inputStreamAsString, file, fileLocation);
-		}).collect(Collectors.toList());
-	}
-
-	private String getFolderName(String module, String tenantId) {
-
-		Calendar calendar = Calendar.getInstance();
-		return getBucketName(tenantId, calendar) + "/" + getFolderName(module, tenantId, calendar);
-	}
-
-	public Resource retrieve(String fileStoreId, String tenantId) throws IOException {
-		return artifactRepository.find(fileStoreId, tenantId);
-	}
-
-	public List<FileInfo> retrieveByTag(String tag, String tenantId) {
-		return artifactRepository.findByTag(tag, tenantId);
-	}
-
-	public Map<String, String> getUrls(String tenantId, List<String> fileStoreIds) {
-
-		Map<String, String> urlMap = getUrlMap(
-				artifactRepository.getByTenantIdAndFileStoreIdList(tenantId, fileStoreIds));
-		if (isNfsStorageEnabled) {
-			for (String s : urlMap.keySet())
-				urlMap.put(s, urlMap.get(s).concat("&tenantId=").concat(tenantId));
-		}
-		return urlMap;
-	}
-
-	private Map<String, String> getUrlMap(List<org.egov.filestore.persistence.entity.Artifact> artifactList) {
-		String src = null;
-		if (isAzureStorageEnabled)
-			src = azureBlobSource;
-		if (isS3Enabled)
-			src = awsS3Source;
-		if (isNfsStorageEnabled)
-			src = diskStorage;
-		final String source = src;
-
-		Map<String, String> mapOfIdAndFile = artifactList.stream()
-				.filter(a -> (null != a.getFileSource() && a.getFileSource().equals(source)))
-				.collect(Collectors.toMap(org.egov.filestore.persistence.entity.Artifact::getFileStoreId,
-						org.egov.filestore.persistence.entity.Artifact::getFileName));
-		return cloudFilesManager.getFiles(mapOfIdAndFile);
-	}
-
-	private String getFolderName(String module, String tenantId, Calendar calendar) {
-		return tenantId + "/" + module + "/" + calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH)
-				+ "/" + calendar.get(Calendar.DATE) + "/";
-	}
-
-	private String getBucketName(String tenantId, Calendar calendar) {
-		// FIXME TODO add config filter logic to create bucket name for qa
-		if (isBucketFixed)
-			return fixedBucketName;
-		else
-			return tenantId.split("\\.")[0] + calendar.get(Calendar.YEAR);
-	}
+  private static final Logger log = LoggerFactory.getLogger(StorageService.class);
+  
+  @Value("${is.bucket.fixed}")
+  private Boolean isBucketFixed;
+  
+  @Value("${fixed.bucketname}")
+  private String fixedBucketName;
+  
+  @Value("${isS3Enabled}")
+  private Boolean isS3Enabled;
+  
+  @Value("${isNfsStorageEnabled}")
+  private Boolean isNfsStorageEnabled;
+  
+  @Value("${isAzureStorageEnabled}")
+  private Boolean isAzureStorageEnabled;
+  
+  @Value("${source.disk}")
+  private String diskStorage;
+  
+  @Value("${source.s3}")
+  private String awsS3Source;
+  
+  @Value("${source.azure.blob}")
+  private String azureBlobSource;
+  
+  @Autowired
+  private CloudFilesManager cloudFilesManager;
+  
+  private static final String UPLOAD_MESSAGE = "Received upload request for jurisdiction: %s, module: %s, tag: %s with file count: %s";
+  
+  private ArtifactRepository artifactRepository;
+  
+  private IdGeneratorService idGeneratorService;
+  
+  @Autowired
+  public StorageService(ArtifactRepository artifactRepository, IdGeneratorService idGeneratorService) {
+    this.artifactRepository = artifactRepository;
+    this.idGeneratorService = idGeneratorService;
+  }
+  
+  public List<String> save(List<MultipartFile> filesToStore, String module, String tag, String tenantId, String inputStreamAsString) {
+    validateFilesToUpload(filesToStore, module, tag, tenantId);
+    log.info("Received upload request for jurisdiction: %s, module: %s, tag: %s with file count: %s", new Object[] { module, tag, Integer.valueOf(filesToStore.size()) });
+    List<Artifact> artifacts = mapFilesToArtifacts(filesToStore, module, tag, tenantId, inputStreamAsString);
+    return this.artifactRepository.save(artifacts);
+  }
+  
+  private void validateFilesToUpload(List<MultipartFile> filesToStore, String module, String tag, String tenantId) {
+    if (CollectionUtils.isEmpty(filesToStore))
+      throw new EmptyFileUploadRequestException(module, tag, tenantId); 
+  }
+  
+  private List<Artifact> mapFilesToArtifacts(List<MultipartFile> files, String module, String tag, String tenantId, String inputStreamAsString) {
+    String folderName = getFolderName(module, tenantId);
+    return (List<Artifact>)files.stream().map(file -> {
+          String fileName = folderName + System.currentTimeMillis() + file.getOriginalFilename();
+          String id = this.idGeneratorService.getId();
+          FileLocation fileLocation = new FileLocation(id, module, tag, tenantId, fileName, null);
+          return new Artifact(inputStreamAsString, file, fileLocation);
+        }).collect(Collectors.toList());
+  }
+  
+  private String getFolderName(String module, String tenantId) {
+    Calendar calendar = Calendar.getInstance();
+    return getBucketName(tenantId, calendar) + "/" + getFolderName(module, tenantId, calendar);
+  }
+  
+  public Resource retrieve(String fileStoreId, String tenantId) throws IOException {
+    return this.artifactRepository.find(fileStoreId, tenantId);
+  }
+  
+  public List<FileInfo> retrieveByTag(String tag, String tenantId) {
+    return this.artifactRepository.findByTag(tag, tenantId);
+  }
+  
+  public Map<String, String> getUrls(String tenantId, List<String> fileStoreIds) {
+    Map<String, String> urlMap = getUrlMap(this.artifactRepository
+        .getByTenantIdAndFileStoreIdList(tenantId, fileStoreIds));
+    if (this.isNfsStorageEnabled.booleanValue())
+      for (String s : urlMap.keySet())
+        urlMap.put(s, ((String)urlMap.get(s)).concat("&tenantId=").concat(tenantId));  
+    return urlMap;
+  }
+  
+  private Map<String, String> getUrlMap(List<Artifact> artifactList) {
+    String src = null;
+    if (this.isAzureStorageEnabled.booleanValue())
+      src = this.azureBlobSource; 
+    if (this.isS3Enabled.booleanValue())
+      src = this.awsS3Source; 
+    if (this.isNfsStorageEnabled.booleanValue())
+      src = this.diskStorage; 
+    String source = src;
+    Map<String, String> mapOfIdAndFile = (Map<String, String>)artifactList.stream().filter(a -> (null != a.getFileSource() && a.getFileSource().equals(source))).collect(Collectors.toMap(Artifact::getFileStoreId, Artifact::getFileName));
+    return this.cloudFilesManager.getFiles(mapOfIdAndFile);
+  }
+  
+  private String getFolderName(String module, String tenantId, Calendar calendar) {
+    return tenantId + "/" + module + "/" + calendar.getDisplayName(2, 2, Locale.ENGLISH) + "/" + calendar
+      .get(5) + "/";
+  }
+  
+  private String getBucketName(String tenantId, Calendar calendar) {
+    if (this.isBucketFixed.booleanValue())
+      return this.fixedBucketName; 
+    return tenantId.split("\\.")[0] + calendar.get(1);
+  }
 }
